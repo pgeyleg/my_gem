dc2c131f2220eb57742745000da5b3944ea17b22
Pema Geyleg <pema.geyleg@gmail.com>
2019-04-04 23:58:06 -0400

    Adds configuration for Simplecov

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,13 @@
 require "bundler/setup"
 require "my_gem"
 
+require 'simplecov'
+SimpleCov.start do
+  add_filter '/bundle/'
+
+  add_group 'Libraries', 'lib'
+end
+
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = ".rspec_status"
